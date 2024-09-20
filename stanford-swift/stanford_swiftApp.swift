9111619914b9c361f958543574d5279e40e27479
Erin <erayreray@gmail.com>
2024-09-20 15:51:24 -0400

    Initial Commit

---
+++ stanford-swift/stanford_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  stanford_swiftApp.swift
+//  stanford-swift
+//
+//  Created by Erin Smith on 9/20/24.
+//
+
+import SwiftUI
+
+@main
+struct stanford_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

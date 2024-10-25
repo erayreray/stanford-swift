644b0d1a07475994321d2a923cde66951e4780b5
Erin <erayreray@gmail.com>
2024-10-25 16:29:50 -0400

    build: adaptive column sizing

--- stanford-swift/EmojiMemoryGame.swift
+++ stanford-swift/EmojiMemoryGame.swift
@@ -11,7 +11,7 @@ class EmojiMemoryGame: ObservableObject {
     private static let emojis = ["😈", "🐈‍⬛", "👻", "🎃","🙀","🧙‍♀️","☠️","🍭","🕷️"]
     
     private static func createMemoryGame() -> MemoryGame<String> {
-        return MemoryGame(numberOfPairsOfCards: 16) { pairIndex in
+        return MemoryGame(numberOfPairsOfCards: 12) { pairIndex in
             if emojis.indices.contains(pairIndex) {
                 return emojis[pairIndex]
             } else {

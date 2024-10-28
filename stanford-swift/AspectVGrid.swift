d92fd05a250c915152e73d5b98e66bd07fe3b424
Erin <erayreray@gmail.com>
2024-10-28 16:29:08 -0400

    build: diy grid

---
+++ stanford-swift/AspectVGrid.swift
@@ -0,0 +1,18 @@
+//
+//  AspectVGrid.swift
+//  stanford-swift
+//
+//  Created by Erin Smith on 10/28/24.
+//
+
+import SwiftUI
+
+struct AspectVGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AspectVGrid()
+}
